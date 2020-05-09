8ec461b22a0c2cb554ad7cb8e671a08ee922ffac
Shep Moore-Berg <shep@Sheps-MacBook-Pro.local>
2020-05-09 01:11:38 -0400

    New webscraper to only scrape root words

--- webscraper.py
+++ webscraper.py
@@ -8,7 +8,8 @@ url = 'http://www.reta-vortaro.de/revo/'
 browser.get(url)
 browser.switch_to.frame('indekso')
 
-words = []
+allwords = []
+boldwords = []
 letters = ['a', 'b', 'c', 'ĉ', 'd', 'e', 'f', 'g', 'ĝ', 'h', 'ĥ', 'i', 'j', 'ĵ',
            'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 'ŝ', 't', 'u', 'ŭ', 'v', 'z']
 
@@ -16,6 +17,10 @@ for letter in letters:
     browser.find_element_by_link_text(letter).click()
     elements = browser.find_element_by_class_name('enhavo').find_elements_by_xpath("//a[contains(@href, '/art/')]")
     for element in elements:
-        words.append((element.text, element.get_attribute('href')))
-    
-pd.DataFrame.from_records(words).to_csv('revo.csv', header=False, index=False)
+        allwords.append((element.text, element.get_attribute('href')))
+    elements = browser.find_element_by_class_name('enhavo').find_elements_by_xpath("//a[contains(@href, '/art/')]/b//ancestor::a")
+    for element in elements:
+        boldwords.append((element.text, element.get_attribute('href')))
+
+pd.DataFrame.from_records(allwords).to_csv('revo.csv', header=False, index=False)
+pd.DataFrame.from_records(boldwords).to_csv('revo_radikoj.csv', header=False, index=False)
