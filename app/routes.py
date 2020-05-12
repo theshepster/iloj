e9b9d567caf5abcfd673e647690f0cd4c94ccadb
Shep Moore-Berg <shep@Sheps-MacBook-Pro.local>
2020-05-12 19:40:35 -0400

    Index function filters out non-letters in query

--- app/routes.py
+++ app/routes.py
@@ -11,17 +11,16 @@ def index():
     radikoj = request.form.get('radikoj')
     results = []
     if query:
+        query = "".join(re.split("[^a-zA-ZĉŝĥĵĝŭĈŜĤĴĜŬ.]*", query)) # only letters from query
         filepath = './revo_radikoj.csv' if radikoj else './revo.csv'
         df = pd.read_csv(filepath, names=['link'], index_col=0)
-        for i in range(len(query)):
+        for i in range(min(len(query),20)):
             match = query[i:] # match only on the terminal slice of the word
             nomatch = query[i-1:i] # don't match if the letter before the match string matches
-            if match == query:
-                rule = match
-            else:
-                rule = '(?<!{nomatch}){match}'.format(nomatch=nomatch, match=match)
-            filtered = df.filter(regex=re.compile('.*{rule}[aeiou]?$'.format(
-                rule=rule), re.IGNORECASE), axis=0)
+            rule = match if match == query else '(?<!{nomatch}){match}'.format(nomatch=nomatch, match=match)
+            ending = '[aeiou]?'
+            filtered = df.filter(regex=re.compile('.*{rule}{ending}$'.format(
+                rule=rule, ending=ending), re.IGNORECASE), axis=0)
             if len(filtered) > 0 : # there were matches
                 result = Result(matchString=match, 
                                 words=filtered.index.to_list(),
